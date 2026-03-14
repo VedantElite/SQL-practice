9a4df3efe0b1c9bb3afe14967be50a34a301585d
VedantElite <vedantrathod.cyb@gmail.com>
2026-03-14 11:44:25 +0530

    Create Day9.sql
    Trying hardcore sql

---
+++ Day9.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+// not null : it is necessary to fill 
+// unique : once use not again, for authentication
