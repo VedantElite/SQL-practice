c5812e36affd890519456623c0a892a703729d15
VedantElite <vedantrathod.cyb@gmail.com>
2026-03-15 14:39:13 +0530

    Create Dat10 Data Types.sql
    Practice Data Type of a date

---
+++ Dat10 Data Types.sql
@@ -0,0 +1,6 @@
+* INT : Integer type, used for whole numbers.
+* VARCHAR(100) : Variable-length string, up to 100 characters.
+* ENUM : A string object with a value chosen from a list of permitted values. eg.gender ENUM('Male', 'Female', 'Other')
+* DATE : Stores date values. eg date_of_birth DATE
+* TIMESTAMP : Stores date and time, automatically set to the current timestamp when a row is created
+* BOOLEAN : Stores TRUE or FALSE values, often used for flags like is_active 
