f015e1000133d4816ad6dea224419bfe9342b71b
Optimus Prime <vedantrathod.cyb@gmail.com>
2026-03-19 23:21:47 +0530

    Implement table listing for hacker_news dataset
    Every dataset is just a collection of tables. You can think of a dataset as a spreadsheet file containing multiple tables, all composed of rows and columns.
    
    We use the list_tables() method to list the tables in the dataset.

---
+++ Day12.sql
@@ -0,0 +1,6 @@
+# List all the tables in the "hacker_news" dataset
+tables = list(client.list_tables(dataset))
+
+# Print names of all tables in the dataset (there are four!)
+for table in tables:  
+    print(table.table_id)
